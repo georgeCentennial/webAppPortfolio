4d741cd97e82522e12ff97421f1a64cdfd9d36f9
georgeCentennial <gcayaby2@my.centennialcollege.ca>
2025-01-29 20:57:12 -0500

    Addednew bootstrap

--- client/src/project.jsx
+++ client/src/project.jsx
@@ -13,31 +13,31 @@ export default function Project(){
         <>
             <div className="container">
                 <h1>My Projects</h1>
-                <div class="row">
-                    <div class="col-sm-4">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">Blind Dog</div>
-                            <div class="panel-body">
+                <div className="row">
+                    <div className="col-sm-4">
+                        <div className="panel panel-default">
+                            <div className="panel-heading">Blind Dog</div>
+                            <div className="panel-body">
                                 <img src={blindDogOutput} width={200} height={200}/>
                                 <p>Agent in park environment. Stops running if there are no more food in the park environment. Continuously moving while there is food.</p>
                                 <p>I modified some of the code.</p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-sm-4">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">Blind Dog Class diagram</div>
-                            <div class="panel-body">
+                    <div className="col-sm-4">
+                        <div className="panel panel-default">
+                            <div className="panel-heading">Blind Dog Class diagram</div>
+                            <div className="panel-body">
                                 <img src={blindDogClassDiagram} width={200} height={200}/>
                                 <p>Class diagram describing Blind Dog.</p>
                                 <p>I created this diagram from scratch.</p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-sm-4">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">Don't quit social media</div>
-                            <div class="panel-body">
+                    <div className="col-sm-4">
+                        <div className="panel panel-default">
+                            <div className="panel-heading">Don't quit social media</div>
+                            <div className="panel-body">
                                 <img src={summary} width={200} height={200}/>
                                 <p>Summary and reverse outline of an article about social media.</p>
                                 <p>I wrote this for comm171.</p>
